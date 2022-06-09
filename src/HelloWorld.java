69af16a7a0145a1e73fee9586e34883727b021a5
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-06-09 15:06:56 -0500

    syn, types and var exercise complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,27 @@
+import java.nio.charset.StandardCharsets;
+
 public class HelloWorld {
     public static void main (String[] args) {
         System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 5;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "I'm learning to code with Java'";
+        System.out.println(myString);
+
+        float myNumber = 3.14f;
+        System.out.println(myNumber);
+
+        int x = 10;
+        int y = 2;
+        System.out.println(x / y);
+        System.out.println(y - x);
+
+
     }
 }
+/*
+ * psvm shortcut for public static void main
+ * sout or souf shortcut for System.out.println or System.out.println respectively.
+ */
