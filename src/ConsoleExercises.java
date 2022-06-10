4e399ff5a119919d387aeb05134bf8d9b756b3ca
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-06-10 11:50:42 -0500

    calc ex update 2

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -9,21 +9,32 @@ public class ConsoleExercises {
     //Scanner Class
     //ex #1
         Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter a integer: ");
+//        int userInput = scanner.nextInt();
+//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+//    //ex #2
+//        System.out.println("Enter three words: ");
+//        String firstWord = scanner.next();
+//        String secondWord = scanner.next();
+//        String thirdWord = scanner.next();
+//        System.out.println("Your words are: --> \"" + firstWord + " " + secondWord + " " + thirdWord +"\" <--");
+//    //ex #3
+//        scanner.nextLine();
+//        System.out.println("Enter a sentence:");
+//        String userSentence = scanner.nextLine();
+//        System.out.println("Here is your sentence: --> \"" + userSentence + "\" <--");
 
-        System.out.print("Enter a integer: ");
-        int userInput = scanner.nextInt();
-        System.out.println("You entered: --> \"" + userInput + "\" <--");
-    //ex #2
-        System.out.println("Enter three words: ");
-        String firstWord = scanner.next();
-        String secondWord = scanner.next();
-        String thirdWord = scanner.next();
-        System.out.println("Your words are: --> \"" + firstWord + " " + secondWord + " " + thirdWord +"\" <--");
-    //ex #3
-        scanner.nextLine();
-        System.out.println("Enter a sentence:");
-        String userSentence = scanner.nextLine();
-        System.out.println("Here is your sentence: --> \"" + userSentence + "\" <--");
+    //Calculate Perimeter
+    //ex #1
+        System.out.println("Enter classroom length, then width:");
+        String userLength = scanner.nextLine();
+        String userWidth = scanner.nextLine();
+        System.out.println("You entered: \"" + userLength + " for the Length, and " + userWidth + " for the Width\"");
+        int intLength = Integer.parseInt(userLength);
+        int intWidth = Integer.parseInt(userWidth);
+        System.out.println("The area of your room is: \"" + intLength * intWidth + "\"");
+        System.out.println("The perimeter of your room is: \"" + ((intLength*2) + (intWidth*2)) + "\"");
 
     }
 }
