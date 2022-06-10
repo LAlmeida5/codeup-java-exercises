037eacc3f7ac7f4fb32ee032f95f4834031dd5c4
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-06-10 16:07:34 -0500

    ex update

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,58 +1,66 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
     //Loop Basics
         // a.While
-        int i = 5;
-        while (i <= 15) {
-            System.out.println("i is:" + i);
-            i++;
-        }
-
-        // b.Do While
-        int x = -2;
-        do {
-            x = x + 2 ;
-            System.out.println(x);
-        }
-        while (x <= 98);
-
-        int y = 100;
-        do {
-            System.out.println(y);
-            y = y - 5;
-        }
-        while (y >= -10);
-
-        long z = 2;
-        do {
-            System.out.println(z);
-           z = z * z;
-        }
-        while(z <= 1000000);
-
-        //c.For
-
-        for(int a = 100; a >= -10; a = a - 5) {
-            System.out.println(a);
-        }
-
-        for(long b=2; b <= 1000000; b = b *  b) {
-            System.out.println(b);
-        }
-
-    //Fizzbuzz
-
-        for(int c = 1; c <= 100; c++) {
-            if (c % 15 == 0) System.out.println("FizzBuzz");
-            else if (c % 5 == 0) System.out.println("Buzz");
-            else if (c % 3 == 0) System.out.println("Fizz");
-            else System.out.println(c);
-        }
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println("i is:" + i);
+//            i++;
+//        }
+//
+//        // b.Do While
+//        int x = -2;
+//        do {
+//            x = x + 2 ;
+//            System.out.println(x);
+//        }
+//        while (x <= 98);
+//
+//        int y = 100;
+//        do {
+//            System.out.println(y);
+//            y = y - 5;
+//        }
+//        while (y >= -10);
+//
+//        long z = 2;
+//        do {
+//            System.out.println(z);
+//           z = z * z;
+//        }
+//        while(z <= 1000000);
+//
+//        //c.For
+//
+//        for(int a = 100; a >= -10; a = a - 5) {
+//            System.out.println(a);
+//        }
+//
+//        for(long b=2; b <= 1000000; b = b *  b) {
+//            System.out.println(b);
+//        }
+//
+//    //Fizzbuzz
+//
+//        for(int c = 1; c <= 100; c++) {
+//            if (c % 15 == 0) System.out.println("FizzBuzz");
+//            else if (c % 5 == 0) System.out.println("Buzz");
+//            else if (c % 3 == 0) System.out.println("Fizz");
+//            else System.out.println(c);
+//        }
 
     //Table of Powers
+        //incomplete...finish this before monday
 
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int userInput = scan.nextInt();
+        System.out.println(userInput);
 
-
+    //Convert to letter grades
+        //complete by monday
 
 
     }
