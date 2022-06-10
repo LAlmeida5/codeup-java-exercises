9bde1241bcc9c79ee8c36a32002a2f8dac1e1b7f
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-06-10 12:27:46 -0500

    exercise complete, Lunch time

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,27 +3,27 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
     //ex #1
-    //   double pi = 3.14159;
-    //   System.out.printf("The value of pi is approximately %.2f", pi  );
+       double pi = 3.14159;
+       System.out.printf("The value of pi is approximately %.2f%n", pi  );
 
     //Scanner Class
     //ex #1
         Scanner scanner = new Scanner(System.in);
-//
-//        System.out.print("Enter a integer: ");
-//        int userInput = scanner.nextInt();
-//        System.out.println("You entered: --> \"" + userInput + "\" <--");
-//    //ex #2
-//        System.out.println("Enter three words: ");
-//        String firstWord = scanner.next();
-//        String secondWord = scanner.next();
-//        String thirdWord = scanner.next();
-//        System.out.println("Your words are: --> \"" + firstWord + " " + secondWord + " " + thirdWord +"\" <--");
-//    //ex #3
-//        scanner.nextLine();
-//        System.out.println("Enter a sentence:");
-//        String userSentence = scanner.nextLine();
-//        System.out.println("Here is your sentence: --> \"" + userSentence + "\" <--");
+
+        System.out.print("Enter a integer: ");
+        int userInput = scanner.nextInt();
+        System.out.println("You entered: --> \"" + userInput + "\" <--");
+    //ex #2
+        System.out.println("Enter three words: ");
+        String firstWord = scanner.next();
+        String secondWord = scanner.next();
+        String thirdWord = scanner.next();
+        System.out.println("Your words are: --> \"" + firstWord + " " + secondWord + " " + thirdWord +"\" <--");
+    //ex #3
+        scanner.nextLine();
+        System.out.println("Enter a sentence:");
+        String userSentence = scanner.nextLine();
+        System.out.println("Here is your sentence: --> \"" + userSentence + "\" <--");
 
     //Calculate Perimeter
     //ex #1
@@ -35,6 +35,14 @@ public class ConsoleExercises {
         int intWidth = Integer.parseInt(userWidth);
         System.out.println("The area of your room is: \"" + intLength * intWidth + "\"");
         System.out.println("The perimeter of your room is: \"" + ((intLength*2) + (intWidth*2)) + "\"");
+//    David's answer
+        System.out.println("Hey what's the length of your room?");
+        int length = Integer.parseInt(scanner.nextLine());
+        System.out.println("Hey what's the width of your room?");
+        int width = Integer.parseInt(scanner.nextLine());
+        System.out.printf("A room with a length of %d and width of %d results in an area of %d and a perimeter of %d",
+                length, width, length * width, (length * 2) + (width * 2));
+
 
     }
 }
