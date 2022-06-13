1bd8e3939facbe7baffd6c7ec664e4a6ec0d654b
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-06-13 13:42:59 -0500

    MethEx start

---
+++ src/MethodsExercises.java
@@ -0,0 +1,18 @@
+public class MethodsExercises {
+    public static int addition(int num1, int num2){
+        return num1 + num2;
+    }
+    public static int subtraction(int num1, int num2){
+        return num1 - num2;
+    }
+    public static int multiplication(int num1, int num2){
+        return num1 * num2;
+    }
+    public static int division(int num1, int num2){
+        return num1 / num2;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
