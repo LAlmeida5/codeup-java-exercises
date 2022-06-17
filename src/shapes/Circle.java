e69ff362656672bec7c95be85d914c5d154fc0d5
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-06-16 22:29:46 -0500

    Circle exercise update

---
+++ src/shapes/Circle.java
@@ -0,0 +1,16 @@
+package shapes;
+
+import java.util.Scanner;
+
+public class Circle {
+    private int radius(int input) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int userInput = scan.nextInt();
+        return userInput;
+    };
+
+    public static void main(String[] args) {
+
+    }
+}
