a888cb3366fa8b5fa61077eaa83aee53bc491bfa
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-06-20 20:21:13 -0500

    More shapes ex. up to #6 completed

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,41 +1,42 @@
 package shapes;
 
-public class Rectangle {
-    protected int length;
-    protected int width;
+public abstract class Rectangle extends Quadrilateral implements Measurable {
 
-    public Rectangle() {
-    }
-
-    public Rectangle(int length, int width) {
-        this.length = length;
-        this.width = width;
-    }
-
-    public int getPerimeter() {
-        System.out.println("Perimeter of the rectangle");
-        return (length * 2) + (width * 2);
-    }
-
-    public int getArea() {
-        System.out.println("Area of the rectangle");
-        return length * width;
-    }
-
-    public int getLength() {
-        return length;
-    }
-
-    public void setLength(int length) {
-        this.length = length;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
+//    protected int length;
+//    protected int width;
+//
+//    public Rectangle() {
+//    }
+//
+//    public Rectangle(int length, int width) {
+//        this.length = length;
+//        this.width = width;
+//    }
+//
+//    public int getPerimeter() {
+//        System.out.println("Perimeter of the rectangle");
+//        return (length * 2) + (width * 2);
+//    }
+//
+//    public int getArea() {
+//        System.out.println("Area of the rectangle");
+//        return length * width;
+//    }
+//
+//    public int getLength() {
+//        return length;
+//    }
+//
+//    public void setLength(int length) {
+//        this.length = length;
+//    }
+//
+//    public int getWidth() {
+//        return width;
+//    }
+//
+//    public void setWidth(int width) {
+//        this.width = width;
+//    }
+//
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,23 +1,23 @@
 package shapes;
 
 public class Square extends Rectangle {
-    protected int side;
-
-    public Square(int side) {
-        super(side, side);
-        this.side = side;
-
-    }
-
-    @Override
-    public int getArea() {
-        System.out.println("Area of the square");
-        return side * side;
-    }
-
-    @Override
-    public int getPerimeter() {
-        System.out.println("Perimeter of the square");
-        return side * 4;
-    }
+//    protected int side;
+//
+//    public Square(int side) {
+//        super(side, side);
+//        this.side = side;
+//
+//    }
+//
+//    @Override
+//    public int getArea() {
+//        System.out.println("Area of the square");
+//        return side * side;
+//    }
+//
+//    @Override
+//    public int getPerimeter() {
+//        System.out.println("Perimeter of the square");
+//        return side * 4;
+//    }
 }
