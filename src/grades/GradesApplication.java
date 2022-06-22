3ef80e64eb29aef3d5329e207155a2d0c1b19cff
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-06-22 12:18:41 -0500

    exercise update

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,7 +1,9 @@
 package grades;
 
+import java.sql.SQLOutput;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class GradesApplication {
     public static void main(String[] args) {
@@ -25,12 +27,35 @@ public class GradesApplication {
         derek.addGrade(97);
         derek.addGrade(100);
 
-        students.put("Bob the Builder", bob);
-        students.put("Sleepy Joe", joe);
-        students.put("Dirty D", derek);
+        students.put("BobtheBuilder22", bob);
+        students.put("Sleepy-Joe19", joe);
+        students.put("DirtyD1", derek);
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome!\n\nHere are the students GitHub usernames: \n");
         for (Map.Entry<String, Student>i:students.entrySet()) {
-            System.out.println(i.getKey());
+            System.out.printf("| " + i.getKey() + " | ");
+        }
+        boolean decision = true;
+
+        while(decision){
+            System.out.println("\n");
+            System.out.println("Which student would you like to see more information on? ");
+            String userInput = scanner.next();
+            if (students.containsKey(userInput)) {
+                System.out.printf("Name: " + students.get(userInput).getName() + "\nGitHub Username: " + userInput +
+                        "\nCurrent GPA: " + students.get(userInput).getGradeAverage());
+            }else{
+                System.out.println("Sorry, there does not appear to be a student with that usernmame. Please check spelling and " +
+                        "try again.");
+                System.out.println("\nWould you like to continue? (Y/N)");
+                String userDecision = scanner.next();
+                if (userDecision.equalsIgnoreCase("n")){
+                     System.out.println("Now exiting....");
+                     decision = false;
+                }
+            }
         }
     }
 }
